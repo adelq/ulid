e21fc021fdfb4a68212d5d740ae7b1f387de00c8
Adel Qalieh <aqalieh95@gmail.com>
2017-06-14 17:02:28 -0400

    Add tests for encode_time

--- src/test.nim
+++ src/test.nim
@@ -14,3 +14,13 @@ suite "encode_random":
 
   test "should return correct length when changed":
     check(encode_random(12).len == 12)
+
+suite "encode_time":
+  test "should return expected encoded result":
+    check("01ARYZ6S41" == encode_time(int(1469918176385), 10))
+
+  test "should change length properly":
+    check("0001AS99AA60" == encode_time(int(1470264322240), 12))
+
+  test "should truncate time if not enough length":
+    check("AS4Y1E11" == encode_time(int(1470118279201), 8))
