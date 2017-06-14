f5a35dfca549dc1218a6b37f79acaac98ccfb616
Adel Qalieh <aqalieh95@gmail.com>
2017-06-14 16:58:13 -0400

    Add tests for encoding random bytes

--- src/test.nim
+++ src/test.nim
@@ -7,3 +7,10 @@ suite "ulid":
 
   test "should return expected encoded time component result":
     check("01ARYZ6S41" == ulid(int(1469918176385))[0..<10])
+
+suite "encode_random":
+  test "should return correct length":
+    check(encode_random().len == 16)
+
+  test "should return correct length when changed":
+    check(encode_random(12).len == 12)
